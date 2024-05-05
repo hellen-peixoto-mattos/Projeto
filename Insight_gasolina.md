c00a491927d88aee306f0baf33b6f202060727c5
${GITHUB_USER} <${GITHUB_MAIL}>
2024-05-05 17:00:56 +0000

    Implementação de insight

---
+++ Insight_gasolina.md
@@ -0,0 +1,7 @@
+
+
+![Gasolina](gasolina.png)
+
+* Insights:
+
+Com a visualização do **gráfico**, podemos analisar que nesta temporada, entre os dias **2** e **9**, não há muitas vendas, provavelmente por não ser dia de pagamento na maioria das empresas. Já nos dias **4**, **5**, **6**, **7** e **8** as vendas já começam a aumentar, sendo o dia **4** o melhor dia de vendas.
--- gasolina.py
+++ gasolina.py
@@ -6,4 +6,10 @@ dados = pd.read_csv('gasolina.csv')
 
 with sns.axes_style('ticks'):
   grafico = sns.lineplot(data=dados, x='dia', y='venda')
-  grafico.set(title='Vendas da gasolina na cidade de São Paulo',xlabel='Dia', ylabel='Preço (médio)')
+  grafico.set(title='Vendas da gasolina na cidade de São Paulo',xlabel='Dia', ylabel='Preço (médio)')* Insights:
+
+* Insight:
+  
+Com a visualização do gráfico, podemos analisar que nesta temporada, entre os dias 2 e 9, não há muitas vendas, provavelmente por não ser dia de pagamento na maioria das empresas. Já nos dias 4, 5, 6, 7 e 8 as vendas já começam a aumentar, sendo o dia 4 o melhor dia de vendas.
+
+
